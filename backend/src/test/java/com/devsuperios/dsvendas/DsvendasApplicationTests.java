51f9d4eed989c710e81c4d045933a6e2579be1e1
Lavinya Correia <lavinya.correia01@gmail.com>
2021-05-03 09:37:50 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devsuperios/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperios.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
