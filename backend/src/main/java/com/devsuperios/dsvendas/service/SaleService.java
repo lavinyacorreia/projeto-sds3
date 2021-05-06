7b1167f6a1eafcfde9e32bd72fc39ee2e34ffd97
Lavinya Correia <lavinya.correia01@gmail.com>
2021-05-06 12:46:52 -0300

    Busca paginada de vendas, evitando repetição no banco (SaleService)

---
+++ backend/src/main/java/com/devsuperios/dsvendas/service/SaleService.java
@@ -0,0 +1,31 @@
+package com.devsuperios.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperios.dsvendas.dto.SaleDTO;
+import com.devsuperios.dsvendas.entities.Sale;
+import com.devsuperios.dsvendas.repositories.SaleRepository;
+import com.devsuperios.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	//Busca paginada de vendas
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
